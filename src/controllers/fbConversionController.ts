44a3b79ba9979c82796594e638b744b3ab515f56
Jhake Inson <jhakeinson2313@gmail.com>
2021-09-14 17:13:32 +0800

    remove static data

--- src/controllers/fbConversionController.ts
+++ src/controllers/fbConversionController.ts
@@ -8,24 +8,6 @@ import EventData from "../types/EventData";
 import CustomData from "../types/CustomData";
 import Content from "../types/Content";
 
-const currentTime = Math.floor(new Date().valueOf() / 1000);
-
-const user: UserData = {
-    fn: hashString('Jhake'),
-    ln: hashString('Inson'),
-    em: hashString('jhake@demo.com'),
-    client_ip_address: '192.168.1.1',
-    ph: hashString('1234-5678'),
-    client_user_agent: 'Test 1/1'
-}
-
-const event: EventData = {
-    "event_name": "PageView",
-    "event_time": currentTime,
-    "user_data": user,
-    "action_source": "website"
-};
-
 const eventAPI = new EventAPI();
 
 const postPageView = async (req: Request, res: Response) => {
@@ -47,21 +29,13 @@ const postPageView = async (req: Request, res: Response) => {
 }
 
 const customEvent = async (req: Request, res: Response) => {
-    const customData: CustomData = {
-        content_name: 'Sample',
-        content_category: 'Demo',
-        contents: [{id: '123', quantity: 1, item_price: 123}],
-        value: 123,
-        currency: 'USD'
-    }
-
-    event.custom_data = customData;
-
-    let data = {};
+    
+    let evt:EventData = req.body;
     let status = 200;
-
+    let data = {};
+    
     try {
-        const response = await eventAPI.postEvent(event);
+        const response = await eventAPI.postEvent(evt);
         data = response.data;
     } catch(err: any) {
         data = err.response.data;
