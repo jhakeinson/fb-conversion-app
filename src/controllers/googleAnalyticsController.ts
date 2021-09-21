be232dcf5cccc4382eba3bb7c58cc43a5edbf992
Jhake Inson <coniemarjhakeinson@gmail.com>
2021-09-22 03:31:44 +0800

    add logic to postGAIdentify

--- src/controllers/googleAnalyticsController.ts
+++ src/controllers/googleAnalyticsController.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express';
 
 import GAPageData from '../types/GAPageData';
+import GAIdentifyData from '../types/GAIdentifyData';
 
 import Analytics, { PageData } from 'analytics';
 // import googleAnalytics from '@analytics/google-analytics';
@@ -55,8 +56,38 @@ const postGAPageView = async (req: Request, res: Response) => {
             .json(payload);
 }
 
-const postGAIdentity = (req: Request, res: Response) => {
+const postGAIdentify = async (req: Request, res: Response) => {
+    let payload: ResponsePayload = {
+        message: '',
+        error: false,
+        status: 200
+    };
 
+    console.log(req.body);
+
+    const idData: GAIdentifyData = req.body;
+
+    await analytics.identify(idData.userId, {
+        email: idData.traits.em,
+        first_name: idData.traits.fn,
+        last_name: idData.traits.ln,
+        phone: idData.traits.ph,
+        public_ip: idData.traits.client_ip_address,
+        user_agent: idData.traits.client_user_agent
+    })
+    .catch(error => {
+        payload.message = error;
+        payload.status = 400;
+        payload.error = true;
+    });
+
+    if(!payload.error) {
+        payload.message = "IdentifyData sent SUCCESS...";
+    }
+
+    return res
+        .status(payload.status)
+        .json(payload);
 }
 
 const postGATrack = (req: Request, res: Response) => {
@@ -66,6 +97,6 @@ const postGATrack = (req: Request, res: Response) => {
 
 export {
     postGAPageView,
-    postGAIdentity,
+    postGAIdentify,
     postGATrack
 }
