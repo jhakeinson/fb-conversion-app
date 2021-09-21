ea9eb981134aa8bbb4ec8f44017ed4e84ecf0e20
Jhake Inson <coniemarjhakeinson@gmail.com>
2021-09-21 16:03:50 +0800

    add logic to postGAPageView

--- src/controllers/googleAnalyticsController.ts
+++ src/controllers/googleAnalyticsController.ts
@@ -1,8 +1,10 @@
 import {Request, Response} from 'express';
 
-import Analytics from 'analytics';
+import SegmentPageView from '../types/SegmentPageView';
+
+import Analytics, { PageData } from 'analytics';
 // import googleAnalytics from '@analytics/google-analytics';
-const googleAnalytics = require('@analytics/google-analytics');
+const googleAnalytics = require('@analytics/google-analytics').default;
 
 
 const analytics = Analytics({
@@ -14,8 +16,37 @@ const analytics = Analytics({
   ]
 });
 
-const postGAPageView = (req: Request, res: Response) => {
+type ResponsePayload = {
+    message: any | undefined;
+    error: boolean | undefined;
+    status: number;
+}
 
+const postGAPageView = async (req: Request, res: Response) => {
+    let payload: ResponsePayload = {
+        message: '',
+        error: false,
+        status: 200
+    };
+
+    try {
+        const pgData: PageData<string> = req.body;
+        console.log(pgData.);
+        await analytics.page(pgData);
+        // console.log(result);
+        payload.message = 'PageData sent SUCCESS...';
+    } catch (error) {
+        // console.log(error);
+        payload.message = error;
+        payload.status = 400;
+        payload.error = true;
+    }
+
+    console.log(payload);
+
+    return res
+            .status(payload.status)
+            .json(payload);
 }
 
 const postGAIdentity = (req: Request, res: Response) => {
